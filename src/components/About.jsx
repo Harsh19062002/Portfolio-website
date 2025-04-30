006e8658a20ca6d3bf07b7295a0f5245db9d6698
Harsh19062002 <harshmahajan1962002@gmail.com>
2025-04-30 15:39:43 +0530

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,11 @@ const About = () => {
             <span className="text-white">I am a </span>
             <span className="text-[#8245ec]">
               <Typewriter
-                words={['Fullstack Developer', 'UI/UX Designer', 'Coder']}
+                words={[
+                  "MERN Stack Developer",
+                  "Frontend Developer",
+                  "web Developer",
+                ]}
                 loop
                 cursor
                 cursorStyle="|"
@@ -37,7 +41,13 @@ const About = () => {
           </h3>
           {/* About Me Paragraph */}
           <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
-            I am a passionate full-stack developer with hands-on experience in building web applications using the MERN stack. As a fresher, I’ve worked on several personal and collaborative projects that demonstrate my ability to create responsive front-end interfaces and robust back-end systems. I’m eager to apply my skills and grow in a professional environment while contributing to impactful and scalable solutions.
+            I am a passionate full-stack developer with hands-on experience in
+            building web applications using the MERN stack. As a fresher, I’ve
+            worked on several personal and collaborative projects that
+            demonstrate my ability to create responsive front-end interfaces and
+            robust back-end systems. I’m eager to apply my skills and grow in a
+            professional environment while contributing to impactful and
+            scalable solutions.
           </p>
           {/* Resume Button */}
           <a
@@ -46,8 +56,8 @@ const About = () => {
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
             style={{
-              background: 'linear-gradient(90deg, #8245ec, #a855f7)',
-              boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
+              background: "linear-gradient(90deg, #8245ec, #a855f7)",
+              boxShadow: "0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec",
             }}
           >
             DOWNLOAD CV
